6ccc6ae7e62c715c4348212ccd88b7e96a172d8b
herley <herley.shaori@gmail.com>
2025-05-08 06:58:34 +0700

    Add ECR resources for model container and clean up deployment script.

--- 2_deploy_external_model_to_sagemaker_realtime_endpoint/lib/2_deploy_external_model_to_sagemaker_realtime_endpoint-stack.ts
+++ 2_deploy_external_model_to_sagemaker_realtime_endpoint/lib/2_deploy_external_model_to_sagemaker_realtime_endpoint-stack.ts
@@ -3,6 +3,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import { SageMakerResources } from './sagemaker';
 import { S3Resources } from './s3';
+import { EcrResources } from './ecr';
 
 export class DeployExternalModelToSagemakerRealtimeEndpointStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,64 +14,9 @@ export class DeployExternalModelToSagemakerRealtimeEndpointStack extends cdk.Sta
       bucketName: `model-bucket-${cdk.Aws.ACCOUNT_ID}-${cdk.Aws.REGION}`,
     });
 
-    // Create IAM role for SageMaker
-    const sageMakerRole = new iam.Role(this, 'SageMakerExecutionRole', {
-      assumedBy: new iam.ServicePrincipal('sagemaker.amazonaws.com'),
-      description: 'Role for SageMaker to access S3 and other services',
-    });
-
-    // Add permissions for CloudWatch, Logs, and EC2
-    sageMakerRole.addToPolicy(new iam.PolicyStatement({
-      actions: [
-        'cloudwatch:PutMetricData',
-        'logs:CreateLogGroup',
-        'logs:CreateLogStream',
-        'logs:PutLogEvents',
-        'ec2:Describe*',
-      ],
-      resources: ['*'],
-    }));
-
-    // Add S3 permissions for model access
-    sageMakerRole.addToPolicy(new iam.PolicyStatement({
-      actions: ['s3:GetObject', 's3:ListBucket'],
-      resources: [
-        s3Resources.bucket.bucketArn,
-        `${s3Resources.bucket.bucketArn}/*`,
-      ],
-    }));
-
-    // Add bucket policy to allow SageMaker role access
-    s3Resources.bucket.addToResourcePolicy(new iam.PolicyStatement({
-      actions: ['s3:GetObject', 's3:ListBucket'],
-      resources: [
-        s3Resources.bucket.bucketArn,
-        `${s3Resources.bucket.bucketArn}/*`,
-      ],
-      principals: [new iam.ArnPrincipal(sageMakerRole.roleArn)],
-    }));
-
-    // Define scikit-learn image URI (ap-southeast-3)
-    const imageUri = '951798379941.dkr.ecr.ap-southeast-3.amazonaws.com/sagemaker-scikit-learn:1.2-1';
-
-    // Construct model URI
-    const modelDataUrl = `s3://${s3Resources.bucket.bucketName}/model.tar.gz`;
-
-    // Create SageMaker resources
-    const sageMakerResources = new SageMakerResources(this, 'SageMaker', {
-      modelDataUrl,
-      executionRole: sageMakerRole,
-      imageUri,
-    });
-
-    // Ensure SageMaker endpoint depends on bucket and role
-    sageMakerResources.node.addDependency(s3Resources.bucket);
-    sageMakerResources.node.addDependency(sageMakerRole);
-
-    // Output the endpoint name
-    new cdk.CfnOutput(this, 'EndpointName', {
-      value: sageMakerResources.endpoint.endpointName!,
-      description: 'Name of the SageMaker endpoint',
+    // Create ECR repository for model container
+    const ecrResources = new EcrResources(this, 'EcrResources', {
+      repositoryName: `model-repository-${cdk.Aws.ACCOUNT_ID}-${cdk.Aws.REGION}`,
     });
   }
 }
