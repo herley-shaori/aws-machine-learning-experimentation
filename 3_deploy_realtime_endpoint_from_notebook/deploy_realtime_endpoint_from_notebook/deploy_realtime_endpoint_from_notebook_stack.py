82e220ee25f95ac5d9ab0060131ad3e71f00f578
herley <herley.shaori@gmail.com>
2025-05-11 22:55:17 +0700

    Remove S3 bucket and lifecycle configuration from SageMaker notebook stack.

--- 3_deploy_realtime_endpoint_from_notebook/deploy_realtime_endpoint_from_notebook/deploy_realtime_endpoint_from_notebook_stack.py
+++ 3_deploy_realtime_endpoint_from_notebook/deploy_realtime_endpoint_from_notebook/deploy_realtime_endpoint_from_notebook_stack.py
@@ -60,42 +60,42 @@ class DeployRealtimeEndpointFromNotebookStack(Stack):
         )
 
         # Create S3 bucket with removal policy
-        notebook_bucket = s3.Bucket(
-            self,
-            "NotebookBucket",
-            removal_policy=RemovalPolicy.DESTROY,
-            auto_delete_objects=True,
-            encryption=s3.BucketEncryption.S3_MANAGED
-        )
-
-        # Grant the notebook role access to the S3 bucket
-        notebook_bucket.grant_read_write(notebook_role)
-
-        # Create SageMaker Lifecycle Configuration to set default S3 bucket
-        lifecycle_config = sagemaker.CfnNotebookInstanceLifecycleConfig(
-            self,
-            "NotebookLifecycleConfig",
-            notebook_instance_lifecycle_config_name="SetDefaultS3Bucket",
-            on_start=[
-                sagemaker.CfnNotebookInstanceLifecycleConfig.NotebookInstanceLifecycleHookProperty(
-                    content=Fn.base64(
-                        f"#!/bin/bash\n"
-                        f"aws s3 sync /home/ec2-user/SageMaker/ s3://{notebook_bucket.bucket_name}/\n"
-                        f"echo 'export SAGEMAKER_DEFAULT_S3_BUCKET={notebook_bucket.bucket_name}' >> /home/ec2-user/.bashrc"
-                    )
-                )
-            ]
-        )
-
-        # Create SageMaker Notebook Instance
-        notebook_instance = sagemaker.CfnNotebookInstance(
-            self,
-            "NotebookInstance",
-            instance_type="ml.t3.2xlarge",
-            role_arn=notebook_role.role_arn,
-            subnet_id=self.public_subnet_1.subnet_id,
-            security_group_ids=[security_group.security_group_id],
-            notebook_instance_name="MyNotebookInstance",
-            direct_internet_access="Enabled",
-            lifecycle_config_name=lifecycle_config.notebook_instance_lifecycle_config_name
-        )
+        # notebook_bucket = s3.Bucket(
+        #     self,
+        #     "NotebookBucket",
+        #     removal_policy=RemovalPolicy.DESTROY,
+        #     auto_delete_objects=True,
+        #     encryption=s3.BucketEncryption.S3_MANAGED
+        # )
+        #
+        # # Grant the notebook role access to the S3 bucket
+        # notebook_bucket.grant_read_write(notebook_role)
+        #
+        # # Create SageMaker Lifecycle Configuration to set default S3 bucket
+        # lifecycle_config = sagemaker.CfnNotebookInstanceLifecycleConfig(
+        #     self,
+        #     "NotebookLifecycleConfig",
+        #     notebook_instance_lifecycle_config_name="SetDefaultS3Bucket",
+        #     on_start=[
+        #         sagemaker.CfnNotebookInstanceLifecycleConfig.NotebookInstanceLifecycleHookProperty(
+        #             content=Fn.base64(
+        #                 f"#!/bin/bash\n"
+        #                 f"aws s3 sync /home/ec2-user/SageMaker/ s3://{notebook_bucket.bucket_name}/\n"
+        #                 f"echo 'export SAGEMAKER_DEFAULT_S3_BUCKET={notebook_bucket.bucket_name}' >> /home/ec2-user/.bashrc"
+        #             )
+        #         )
+        #     ]
+        # )
+        #
+        # # Create SageMaker Notebook Instance
+        # notebook_instance = sagemaker.CfnNotebookInstance(
+        #     self,
+        #     "NotebookInstance",
+        #     instance_type="ml.t3.2xlarge",
+        #     role_arn=notebook_role.role_arn,
+        #     subnet_id=self.public_subnet_1.subnet_id,
+        #     security_group_ids=[security_group.security_group_id],
+        #     notebook_instance_name="MyNotebookInstance",
+        #     direct_internet_access="Enabled",
+        #     lifecycle_config_name=lifecycle_config.notebook_instance_lifecycle_config_name
+        # )
