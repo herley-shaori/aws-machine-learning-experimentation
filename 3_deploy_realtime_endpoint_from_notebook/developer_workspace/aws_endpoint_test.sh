4507059e74e29f9c76caf841e1e1a859ed716024
herley <herley.shaori@gmail.com>
2025-05-12 16:30:05 +0700

    Add script to test SageMaker endpoint with sample input.

---
+++ 3_deploy_realtime_endpoint_from_notebook/developer_workspace/aws_endpoint_test.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+cat > input.json << 'EOF'
+[[1.0, 2.0, 3.0]]
+EOF
+
+aws sagemaker-runtime invoke-endpoint \
+    --endpoint-name sklearn-linear-regression-endpoint-kj78 \
+    --body file://input.json \
+    --content-type application/json \
+    --accept application/json \
+    --region ap-southeast-3 \
+    --cli-binary-format raw-in-base64-out \
+    output.json
+
+cat output.json
